56db30d5e9586fb8852817dff2dea11c94cc780f
suzaku <89909040+suzaku01@users.noreply.github.com>
2023-10-28 12:41:21 +0900

    Update sys_session.go

--- sys_session.go
+++ sys_session.go
@@ -31,55 +31,21 @@ func (s *Session) Start() {
 func (s *Session) recvLoop() {
 	for {
 		if time.Now().Add(-30 * time.Second).After(s.lastPacket) {
-	url := "http://localhost:9998/delete_user"
-	// POSTリクエストの実行
-	resp, err := http.Post(url, "application/json", strings.NewReader("{}"))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
 			logoutPlayer(s)
 			return
 		}
 		if s.closed {
-	url := "http://localhost:9998/delete_user"
-	// POSTリクエストの実行
-	resp, err := http.Post(url, "application/json", strings.NewReader("{}"))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
 			logoutPlayer(s)
 			return
 		}
 		pkt, err := s.cryptConn.ReadPacket()
 		if err == io.EOF {
-			s.logger.Info(fmt.Sprintf("[%s] Disconnected", s.Name))
-	
-	url := "http://localhost:9998/delete_user"
-	// POSTリクエストの実行
-	resp, err := http.Post(url, "application/json", strings.NewReader("{}"))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
-			
+			s.logger.Info(fmt.Sprintf("[%s] Disconnected", s.Name))		
 			logoutPlayer(s)
 			return
 		}
 		if err != nil {
 			s.logger.Warn("Error on ReadPacket, exiting recv loop", zap.Error(err))
-	url := "http://localhost:9998/delete_user"
-	// POSTリクエストの実行
-	resp, err := http.Post(url, "application/json", strings.NewReader("{}"))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
 			logoutPlayer(s)
 			return
 		}
